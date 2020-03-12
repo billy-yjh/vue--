9feedbd52d921b4a32b20649781e6289f714ecb2
e <940340942@qq.com>
2020-03-12 16:50:15 +0800

    feat(master):初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      input: 'text'
+    }
   }
 }
 </script>
